6746c88c86fc7cb5a5ce9a125b5a986b86b246f4
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-05-28 00:27:05 +0300

    finished post router setup

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,17 +2,6 @@ import Post from '../model/Post.js';
 import User from '../model/User.js';
 
 /*  CREATE  */
-export const getFeedPosts = async(req, res) => {
-    try {
-        const post = await Post.find();
-        res.status(200).json(post);
-
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
-/*  READ  */
 export const createPost = async(req, res) => {
     try {
         const { userId, description, picturePath } = req.body;
@@ -37,5 +26,52 @@ export const createPost = async(req, res) => {
 
     } catch (error) {
         res.status(409).json({ message: error.message })
+    }
+}
+
+/*  READ  */
+export const getFeedPosts = async(req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(200).json(post);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getUserPosts = async(req, res) => {
+    try {
+        const { userId } = req.params;
+        const userPosts = await Post.find({ userId });
+        res.status(200).json(userPosts);
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+
+/*  UPDATE  */
+export const likePost = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if(isLiked) {
+            post.likes.delete(userId)
+        } else {
+            post.likes.set(userId, true)
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true }
+        )
+
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(404).json({message: error.message})
     }
 }
